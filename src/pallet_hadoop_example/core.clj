75930a4a5c89494e965cea89abd0fd2da1fc9ebb
Sam Ritchie <sritchie09@gmail.com>
2011-05-30 23:14:20 -0400

    Further renaming of create and destroy methods.

--- src/pallet_hadoop_example/core.clj
+++ src/pallet_hadoop_example/core.clj
@@ -10,6 +10,10 @@
   {:algorithms {:lift-fn pallet.core/parallel-lift
                 :converge-fn pallet.core/parallel-adjust-node-counts}})
 
+(def ec2-service (compute/compute-service "aws-ec2"
+                                          :identity "ec2-access-key-id"
+                                          :credential "ec2-secret-access-key"))
+
 (def ec2-service
   (compute/compute-service-from-config-file :aws))
 
@@ -17,17 +21,14 @@
   (cluster-spec :private
                 {:jobtracker (hadoop-node [:jobtracker :namenode])
                  :slaves (slave-node 3)}
-                :base-machine-spec {
-                                    :os-family :ubuntu
+                :base-machine-spec {:os-family :ubuntu
                                     :os-version-matches "10.10"
                                     :os-64-bit true
-                                    :fastest true
-                                    :spot-price (float 1.60)
-                                    }
+                                    :fastest true}
                 :base-props {:hdfs-site {:dfs.data.dir "/mnt/dfs/data"
                                          :dfs.name.dir "/mnt/dfs/name"}
                              :mapred-site {:mapred.task.timeout 300000
-                                           :mapred.reduce.tasks (int (* 1.2 15 nodecount))
+                                           :mapred.reduce.tasks 60
                                            :mapred.tasktracker.map.tasks.maximum 15
                                            :mapred.tasktracker.reduce.tasks.maximum 15
                                            :mapred.child.java.opts "-Xms1024m -Xmx1024m"}}))
@@ -40,10 +41,19 @@
                :group hadoop-user
                :mode "0755"))
 
-(defn forma-boot []
-  (do (boot-cluster test-cluster :compute ec2-service :environment remote-env)
-      (lift-cluster test-cluster authorize-mnt :compute ec2-service :environment remote-env)
-      (start-cluster test-cluster :compute ec2-service :environment remote-env)))
+(defn create-cluster []
+  (do (boot-cluster test-cluster
+                    :compute ec2-service
+                    :environment remote-env)
+      (lift-cluster test-cluster
+                    authorize-mnt
+                    :compute ec2-service
+                    :environment remote-env)
+      (start-cluster test-cluster
+                     :compute ec2-service
+                     :environment remote-env)))
 
-(defn forma-kill []
-  (kill-cluster forma-cluster :compute env/ec2-service :environment env/remote-env))
+(defn destroy-cluster []
+  (kill-cluster test-cluster
+                :compute ec2-service
+                :environment remote-env))
