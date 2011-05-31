a0201092b65ab598d379cc69576910db9163339e
Sam Ritchie <sritchie09@gmail.com>
2011-05-31 10:54:13 -0400

    upgraded project.clj to include jclouds stuff in dev dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject pallet-hadoop-example "0.0.1-SNAPSHOT"
   :description "Example project for running Hadoop on Pallet."
   :repositories {"conjars" "http://conjars.org/repo/"
-                 "sonatype-release"
-                 "http://oss.sonatype.org/content/repositories/releases/"
-                 "sonatype-snap"
-                 "http://oss.sonatype.org/content/repositories/snapshots/"}
+                 "sonatype"
+                 "http://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[pallet-hadoop "0.0.2-SNAPSHOT"]
+  :dev-dependencies [[pallet-hadoop "0.1.0"]
+                     [org.jclouds/jclouds-all "1.0-beta-9c"]
+                     [org.jclouds.driver/jclouds-jsch "1.0-beta-9c"]
+                     [org.jclouds.driver/jclouds-log4j "1.0-beta-9c"]
+                     [log4j/log4j "1.2.14"]
                      [swank-clojure "1.4.0-SNAPSHOT"]
                      [clojure-source "1.2.0"]])
