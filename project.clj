6c0ee26a4bf06d97e95c3ab07c6037da5f11944d
Sam Ritchie <sritchie09@gmail.com>
2011-06-01 00:11:20 -0400

    removed conjars repository. Moved pallet-hadoop to dev dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject pallet-hadoop-example "0.0.1-SNAPSHOT"
   :description "Example project for running Hadoop on Pallet."
   :main pallet-hadoop-example.core
-  :repositories {"conjars" "http://conjars.org/repo/"
-                 "sonatype"
+  :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[pallet-hadoop "0.1.0"]
+  :dev-dependencies [[pallet-hadoop "0.1.1"]
                      [org.jclouds/jclouds-all "1.0-beta-9c"]
                      [org.jclouds.driver/jclouds-jsch "1.0-beta-9c"]
                      [org.jclouds.driver/jclouds-log4j "1.0-beta-9c"]
