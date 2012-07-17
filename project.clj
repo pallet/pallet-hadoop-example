2dbd1b85da30ddd19c33f4ea20a07c2940d43f70
Antoni Batchelli <tbatchelli@acm.org>
2012-07-17 14:30:22 -0700

    Make the project compatible with both lein 1 and lein 2.

--- project.clj
+++ project.clj
@@ -2,12 +2,11 @@
   :description "Example project for running Hadoop on Pallet."
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases/"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[org.cloudhoist/pallet-hadoop "0.3.3-beta.4"]
-                     [org.jclouds/jclouds-all "1.4.1"]
-                     [org.jclouds.driver/jclouds-sshj "1.4.1"]
-                     [org.jclouds.driver/jclouds-slf4j "1.4.1"]
-                     [org.cloudhoist/pallet-jclouds "1.4.0-beta.1"]
-                     [ch.qos.logback/logback-classic "1.0.1"]
-                 ;;    [com.jcraft/jsch "0.1.48"]
-                     [ch.qos.logback/logback-core "1.0.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.cloudhoist/pallet-hadoop "0.3.3-beta.4"]
+                 [org.jclouds/jclouds-all "1.4.1"]
+                 [org.jclouds.driver/jclouds-sshj "1.4.1"]
+                 [org.jclouds.driver/jclouds-slf4j "1.4.1"]
+                 [org.cloudhoist/pallet-jclouds "1.4.0-beta.1"]
+                 [ch.qos.logback/logback-classic "1.0.1"]
+                 [ch.qos.logback/logback-core "1.0.1"]])
