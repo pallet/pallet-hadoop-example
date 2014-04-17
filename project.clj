9ac77f3485dfed89c84010e5edfb5173577a273f
Tom Faulhaber <git_com@infolace.com>
2014-04-17 07:13:18 -0700

    Update the jclouds dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.cloudhoist/pallet-hadoop "0.3.3-beta.4"]
-                 [org.jclouds/jclouds-all "1.4.1"]
-                 [org.jclouds.driver/jclouds-sshj "1.4.1"]
-                 [org.jclouds.driver/jclouds-slf4j "1.4.1"]
-                 [org.cloudhoist/pallet-jclouds "1.4.0-beta.1"]
+                 [org.cloudhoist/pallet-hadoop "0.3.3"]
+                 [org.apache.jclouds/jclouds-all "1.7.1"]
+                 [org.apache.jclouds.driver/jclouds-sshj "1.7.1"]
+                 [org.apache.jclouds.driver/jclouds-slf4j "1.7.1"]
+                 [com.palletops/pallet-jclouds "1.7.0"]
                  [ch.qos.logback/logback-classic "1.0.1"]
                  [ch.qos.logback/logback-core "1.0.1"]])
